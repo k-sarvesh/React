c4a7b5540ef8de953a7ce41b00784f0fbba475cb
k-sarvesh <sarveshkhawale17@gmail.com>
2026-03-29 23:26:06 +0530

    functions done

--- 06-functions/src/App.jsx
+++ 06-functions/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+
 function App() {
+  function btnclicked(){
+  console.log("Button clicked")
+}
+function mouseEnter(){
+  console.log("Mouse Entered");
+}
+function typeing(){
+  console.log("Typing");
+}
   return (
-    <div>App</div>
+    <div className="">
+      <button onMouseEnter={mouseEnter} onClick={btnclicked}>Click me</button>
+      <input onChange={(elem)=>{
+        typeing();
+        console.log(elem.target.value);
+      }} type="text" placeholder='Enter somthing' />
+    </div>
   )
 }
 
